f3f1b233596c74701003217226fa27ada1d73a39
Srinath Bathina <Srinath.Bathina@infor.com>
2023-09-09 22:49:51 +0530

    Fixed status check code

--- common.sh
+++ common.sh
@@ -20,8 +20,8 @@ enable_service_restart() {
   status_check
 }
 
-status_check(){
-  if [$? eq 0]; then
+status_check() {
+  if [ $? -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILED\e[0m"
